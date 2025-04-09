bd1df2b5a4fcbc360b491e02a8b058f2ab4d0584
sillypartyman <sillypartyman330@gmail.com>
2025-04-09 10:53:32 -0600

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,7 @@ function About() {
       <h1 className="text-4xl font-bold text-white mb-8">About</h1>
       <div className="bg-gray-900 rounded-lg p-8">
         <p className="text-gray-300 mb-4">
-          Welcome to my digital portfolio! My name is Shawn Smith and this website is made to show
-          off the work I did in class.
+          Hi, my name is Shawn Smith, I'm a computer science major and I really enjoy building new things.
         </p>
        
       </div>
